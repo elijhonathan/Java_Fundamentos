aac544a016267246dded11cdb2e5d4f1d578514a
elijhonathan <eligrancej@hotmail.com>
2024-09-15 16:48:17 -0300

    ok

---
+++ 03-Conversoes.java
@@ -0,0 +1,11 @@
+package First_HelloWorld;
+
+public class Conversoes {
+	
+	public static void main(String[]args) {
+		double valorDouble = 9.99;
+		int valorInt = (int) valorDouble;
+		System.out.println(valorInt);
+	}
+
+}
